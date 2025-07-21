a8c8edd85e1423224c7a0db541c6d2010d343d79
iamVihanga <vihangarashansilva@gmail.com>
2025-07-21 18:31:54 +0530

    Initial Commit: Project setup with React, TailwindCSS and Shadcn UI

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.body);
+
+root.render(
+  <h2 className="text-2xl font-semibold text-cyan-600">Hello from React!</h2>
+);
