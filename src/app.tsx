531b08cdbf0942c76c008843fce0c398d1444632
iamVihanga <vihangarashansilva@gmail.com>
2025-08-23 15:34:32 +0530

    Fix: Eslint error - Path Aliases

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Button } from "./components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 const root = createRoot(document.body);
 
 root.render(
   <h2 className="text-2xl font-semibold text-cyan-600">
-    Hello from React!
+    <Badge>Hello</Badge> from React!
     <Button>Hello World</Button>
   </h2>
 );
